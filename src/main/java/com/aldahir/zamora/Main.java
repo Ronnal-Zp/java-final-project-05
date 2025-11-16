afeb993cd23382703f4190ee5c78526ff84001bd
Aldahir Zamora <ronn.zp.3@gmail.com>
2025-11-16 14:37:31 -0500

    configurar arrancado de proyecto

--- src/main/java/com/aldahir/zamora/Main.java
+++ src/main/java/com/aldahir/zamora/Main.java
@@ -1,7 +1,22 @@
 package com.aldahir.zamora;
 
+import com.aldahir.zamora.product.controller.ProductController;
+import com.aldahir.zamora.product.repository.ProductRepository;
+import com.aldahir.zamora.product.service.ProductService;
+import com.aldahir.zamora.product.view.ProductView;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ProductRepository productRepository = new ProductRepository();
+        ProductService productService = new ProductService(productRepository);
+        ProductController productController = new ProductController(productService);
+        Scanner scanner = new Scanner(System.in);
+
+        ProductView productView = new ProductView(productController,scanner);
+        productView.start();
+
     }
 }
